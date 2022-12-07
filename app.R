3e1aa9c9d8df3bfcdbbbcab8185fe4d51e2ac104
wastjohn <st.john.will@gmail.com>
2022-12-07 13:49:49 -0600

    Added in scatter plot and initial tree logic

--- app.R
+++ app.R
@@ -2,46 +2,88 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(rpart)
+library(rpart.plot)
 library(DT)
 
 majors <- read_csv("https://raw.githubusercontent.com/wastjohn/COMP112-Final/main/majordata.csv")
 
+f_gen <- majors %>%
+  filter(first_gen == 'first gen')
+
 ui <- fluidPage(
+    #theme = bslib::bs_theme(bootswatch = "darkly"),
 
     titlePanel('Macalester Major Data'),
     
     tabsetPanel(type = "tabs",
-                tabPanel("Scatter Plot", 
-                         plotOutput("fgen_plot"),
-                         selectInput('x_var', label = 'First Gen X Variable', choices = c(names(majors))),
-                         selectInput('y_var', label = 'First Gen Y variable', choices = c(names(majors))),
-                         plotOutput("plot"),
+                tabPanel("Bar Plot",
+                         fluidRow(
+                           column(6, plotOutput("fgen_bar_plot")),
+                           column(6, plotOutput("bar_plot"))
+                           ),
+                         
+                         fluidRow(
+                           column(12,
+                                  selectInput('x_var', label = 'X Variable', choices = c(names(majors))))
+                           ), align = 'center'
+                         
                          ),
                 tabPanel("Classification Tree", 
                          plotOutput("treeplot"),
-                         selectInput('filter_opt', label = 'Filter Options', choices = c(names(majors)), multiple = TRUE)
+                         selectInput('filter_opt', label = 'Filter Options', choices = c(names(majors)), multiple = TRUE,selected = c('finpuirst_gen'))
                          
-                         )
+                         ),
+                tabPanel("Scatter Plot", 
+                         fluidRow(
+                           column(6, plotOutput("fgen_scatter_plot")),
+                           column(6, plotOutput("scatter_plot"))
+                         ),
+                         
+                         fluidRow(
+                           column(12,
+                                  selectInput('x_scatter_var', label = 'X Variable', choices = c(names(majors))),
+                                  selectInput('y_scatter_var', label = 'Y Variable', choices = c(names(majors)))
+                                  )
+                         ), align = 'center'
+                         
+                ),
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
 
-  output$fgen_plot <- renderPlot({
+  output$fgen_bar_plot <- renderPlot({
     
     ggplot(f_gen, aes(x = .data[[input$x_var]]))+
       geom_bar() +
+      labs(title = 'First Gen Bar Plot')
+    
+  })
+  
+  output$bar_plot <- renderPlot({
+    
+    ggplot(majors, aes(x = .data[[input$x_var]]))+
+      geom_bar() +
+      labs(title = 'Total Major Bar Plot')
+  
+  })
+  
+  output$fgen_scatter_plot <- renderPlot({
+    
+    ggplot(f_gen, aes(x = .data[[input$x_scatter_var]], y = .data[[input$y_scatter_var]]))+
+      geom_point() +
       labs(title = 'First Gen Scatter Plot')
     
   })
   
-  output$plot <- renderPlot({
+  output$scatter_plot <- renderPlot({
     
-    ggplot(majors, aes(x = .data[[input$x_var]]))+
-      geom_bar() +
+    ggplot(majors, aes(x = .data[[input$x_scatter_var]], y = .data[[input$y_scatter_var]]))+
+      geom_point() +
       labs(title = 'Total Major Scatter Plot')
-  
+    
   })
   
   output$treeplot <- renderPlot({
@@ -53,8 +95,11 @@ server <- function(input, output, session) {
     
     first_gen_major <- first_gen_major[shuffle_index, ]
     
-    clean_first_gen <- first_gen_major %>%
-      select(c(first_gen, sex, tot_enroll_ct, prefix_count, major1_division))
+    #clean_first_gen <- first_gen_major %>%
+     # select(c(first_gen, sex, tot_enroll_ct, prefix_count, major1_division))
+    #input$filter_opt
+    browser()
+    clean_first_gen <- first_gen_major[c('sex',input$filter_opt)]
     
     create_train_test <- function(data, size = 0.8, train = TRUE) {
       n_row = nrow(data)
